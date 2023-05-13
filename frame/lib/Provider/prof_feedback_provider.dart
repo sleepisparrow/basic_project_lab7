367db544611fe6217a7083ca067558d9d89fffd5
YeonWoo Bae <sleepisparrow@gmail.com>
2023-05-13 18:26:34 +0900

    make provider, add feedback function function: add feedback to provider

---
+++ frame/lib/Provider/prof_feedback_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfFeedbackProvider with ChangeNotifier {
+  List<int> _choices = List.filled(5, 0);
+  List<String> _feedback = List.empty(growable: true);
+
+  List<int> get choices => _choices;
+  List<String> get feedback => _feedback;
+
+  void addFeedback(String value) {
+    _feedback.add(value);
+  }
+}
