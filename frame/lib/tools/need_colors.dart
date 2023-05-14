8ae4233195ab9e9b1f91e8c5d8e20c332608cdad
0314PILOVE <chunhe50@naver.com>
2023-05-14 18:33:40 +0900

    변경점
    need_colors.dart 파일 생성 후, 필요한 색상 집어넣음

---
+++ frame/lib/tools/need_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class NeedColors{
+  static const Color darkBlue = Color(0xff11307C);
+  static const Color lightGrey = Color(0xffE3E5EE);
+  static const Color darkGrey = Color(0xffA2ABCE);
+}
