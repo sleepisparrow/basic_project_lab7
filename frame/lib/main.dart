b69b6647d7e7439dde6f5e28192c8f20f737ff8d
hagisilm <catchwall3203@naver.com>
2023-05-15 19:47:46 +0900

    마지막 수정

--- frame/lib/main.dart
+++ frame/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frame/Provider/Prof_quiz_main_provider.dart';
 import 'package:frame/Provider/prof_feedback_provider.dart';
 import 'package:frame/screen/std_feedback.dart';
 import 'package:frame/screen/prof_quiz_main_page.dart';
@@ -9,22 +10,20 @@ import 'screen/login_screen.dart';
 
 void main() {
   runApp(
-     MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue[900],
-        cardColor: Colors.grey[100],
-      ),
-      home: LoginScreen(),
     MultiProvider(
       providers: [
         ChangeNotifierProvider<ProfFeedbackProvider>(
             create: (_) => ProfFeedbackProvider()),
         ChangeNotifierProvider<ProfQuizMainProvider>(
-            create: (_) =>ProfQuizMainProvider()),
+            create: (_) => ProfQuizMainProvider()),
       ],
       child: MaterialApp(
-        home: ProfQuizMainPage(),
-      )
+        theme: ThemeData(
+          primaryColor: Colors.blue[900],
+          cardColor: Colors.grey[100],
+        ),
+        home: LoginScreen(),
+      ),
     ),
   );
 }
