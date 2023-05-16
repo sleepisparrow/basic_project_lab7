73b0ee7e3be3ef454500ae3ed9f4bd575c1cc73f
YeonWoo Bae <sleepisparrow@gmail.com>
2023-05-16 15:56:26 +0900

    solve error: professor feedback page

--- frame/lib/Page/quiz_page.dart
+++ frame/lib/Page/quiz_page.dart
@@ -6,6 +6,25 @@ class QuizPage extends StatelessWidget {
   ///네비게이션바의 질문눌렀을때 퀴즈페이지
   @override
   Widget build(BuildContext context) {
-    return Text('quiz');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: const Text(
+              '퀴즈는 아직 공개되지 않았습니다.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          const SizedBox(
+            width: 212,
+            child: Image(
+              image: AssetImage('asset/lock.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
