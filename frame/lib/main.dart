d73485bba9ca8582c7baa4cc61a50fef9548d37f
YeonWoo Bae <sleepisparrow@gmail.com>
2023-05-16 19:59:16 +0900

    stu_quiz_background_page.dart - text를 잘 보여주는가? with test code.

--- frame/lib/main.dart
+++ frame/lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frame/Provider/Prof_quiz_main_provider.dart';
 import 'package:frame/Provider/prof_feedback_provider.dart';
-import 'package:frame/screen/std_feedback.dart';
-import 'package:frame/screen/prof_quiz_main_page.dart';
 import 'package:provider/provider.dart';
 import 'package:frame/screen/login_screen.dart';
-import 'package:frame/screen/prof_feedback_page.dart';
-import 'screen/login_screen.dart';
 
 void main() {
   runApp(
