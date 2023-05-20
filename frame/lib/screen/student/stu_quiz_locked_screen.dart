0255004c7ecff4ba7a2f89ea445ca52fcee5af4d
성원준 <wseong06@gmail.com>
2023-05-20 13:33:27 +0900

    pro_question_screen modified
    <Modification>
    1. stu_quiz_locked_screen generated
    location : frame\lib\screen\student

---
+++ frame/lib/screen/student/stu_quiz_locked_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class StuQuizLockedScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Center(
+          child: Column(
+
+          ),
+        ),
+      ),
+    );
+  }
+}
